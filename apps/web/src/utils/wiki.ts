b68e4595a9b719162e05c3703480efd572aa6de2
freeislet <freeislet@gmail.com>
2025-07-30 17:23:11 +0900

    @ alias 적용

--- apps/web/src/utils/wiki.ts
+++ apps/web/src/utils/wiki.ts
@@ -2,11 +2,6 @@ import { getEntry } from 'astro:content'
 import { marked } from 'marked'
 import { getWikiFromR2 } from './r2Client'
 
-// Base URLs for different wiki content sources (참고용)
-const _SOURCES = {
-  R2: 'https://static.ttsg.dev/wiki', // Cloudflare R2 path (placeholder)
-}
-
 export interface WikiContent {
   title: string
   content: string
