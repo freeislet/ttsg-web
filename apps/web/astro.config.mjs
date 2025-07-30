758a6d847313bb88dc13caf467b5db0cdbb54311
freeislet <freeislet@gmail.com>
2025-07-31 06:08:40 +0900

    function 배포 오류 해결

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -14,8 +14,14 @@ export default defineConfig({
   vite: {
     resolve: {
       alias: {
-        '@': path.resolve('./src')
-      }
-    }
-  }
+        '@': path.resolve('./src'),
+        ...(import.meta.env.PROD && {
+          // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+          // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+          // (https://github.com/withastro/astro/issues/12824#issuecomment-2563095382)
+          'react-dom/server': 'react-dom/server.edge',
+        }),
+      },
+    },
+  },
 })
