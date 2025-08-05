32968d8a9634d15ef59df6ab3d1401a70286bccd
freeislet <freeislet@gmail.com>
2025-08-05 23:49:36 +0900

    redirect 주소 변경

--- apps/web-redirect/functions/[[path]].js
+++ apps/web-redirect/functions/[[path]].js
@@ -1,6 +1,6 @@
 /**
  * Cloudflare Pages Functions 모든 경로에 대한 함수
- * ttsg.pages.dev에서 ttsg.freeislet.workers.dev로의 301 리디렉션을 처리합니다.
+ * ttsg.pages.dev에서 ttsg.space로의 301 리디렉션을 처리합니다.
  * 모든 요청 경로를 새 도메인으로 리디렉션합니다.
  */
 
@@ -8,7 +8,7 @@
 export function onRequest(context) {
   const request = context.request
   const url = new URL(request.url)
-  const targetUrl = 'https://ttsg.freeislet.workers.dev' + url.pathname + url.search
+  const targetUrl = 'https://ttsg.space' + url.pathname + url.search
 
   // 301 Moved Permanently 상태 코드로 리디렉션
   return Response.redirect(targetUrl, 301)
